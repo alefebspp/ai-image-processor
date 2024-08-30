d9f7954255eee6f3c2363a21890fd4995289803b
Alefe Bispo <alefe.bispo@onesight.com.br>
2024-08-30 20:48:22 -0300

    feat: ensure parse of envs and ensure app only starts if envs are defineds

---
+++ src/env/index.ts
@@ -0,0 +1,21 @@
+import 'dotenv/config'
+import { z } from 'zod'
+
+const envSchema = z.object({
+  GEMINI_API_KEY: z.string(),
+  API_URL: z.string(),
+  DATABASE_HOST:z.string(),
+  DATABASE_USER: z.string(),
+  DATABASE_PASSWORD:z.string(),
+  DATABASE_NAME:z.string(),
+})
+
+const _env = envSchema.safeParse(process.env)
+
+if (_env.success === false) {
+  console.error('Invalid environment variables', _env.error.format())
+
+  throw new Error('Invalid environment variables')
+}
+
+export const env = _env.data

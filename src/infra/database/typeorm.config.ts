1be728661796c47cc3a0999fa31fdba1433807b9
Alefe Bispo <alefe.bispo@onesight.com.br>
2024-08-30 20:43:12 -0300

    update typeorm config

--- src/infra/database/typeorm.config.ts
+++ src/infra/database/typeorm.config.ts
@@ -1,20 +1,46 @@
-import { DataSource } from "typeorm"
-import { Photo } from "../../domain/models/Photo"
+import { DataSource } from 'typeorm';
+import { Measurement } from '../../models/Measurement';
+import { env } from '../../env';
+
+const MAX_RETRIES = 10;
+const RETRY_DELAY_MS = 5000;
+
+let isInitialized = false;
+
+async function initializeDatabase() {
+    if (isInitialized) {
+        return;
+    }
+
+    let retries = 0;
+
+    while (retries < MAX_RETRIES) {
+        try {
+            await AppDataSource.initialize();
+            isInitialized = true;
+            console.log("Successfully connected to database");
+            return;
+        } catch (error) {
+            console.error('Database connection failed, retrying...');
+            retries++;
+            await new Promise((resolve) => setTimeout(resolve, RETRY_DELAY_MS));
+        }
+    }
+
+    console.error('Unable to connect to the database after multiple attempts');
+    process.exit(1);
+}
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
+    host: env.DATABASE_HOST,
     port: 5432,
-    username: "user",
-    password: "user",
-    database: "shopper",
-    entities: [Photo],
+    username: env.DATABASE_USER,
+    password: env.DATABASE_PASSWORD,
+    database: env.DATABASE_NAME,
+    entities: [Measurement],
     synchronize: true,
     logging: false,
-})
+});
 
-AppDataSource.initialize()
-    .then(() => {
-        console.log("Successfully connected to database")
-    })
-    .catch((error) => console.log(error))
+initializeDatabase();
